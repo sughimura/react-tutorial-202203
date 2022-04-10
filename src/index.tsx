fc4e5e0a32e95dc470510ec3ab62b5c6cff3f985
sughimura <nxsughi@gmail.com>
2022-04-10 18:31:01 +0900

    refactor(Board): change to function Component
    add BoardProps

--- src/index.tsx
+++ src/index.tsx
@@ -35,37 +35,37 @@ const calculateWinner = (squares: SquaresType) => {
   return null;
 }
 
-
-class Board extends React.Component {
-  renderSquare(i) {
+interface BoardProps {
+  squares: SquaresType;
+  onClick: (i: number) => void;
+}
+const Board: React.VFC<BoardProps> = (props) => {
+  const renderSquare = (i: number) => {
     return <Square
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
+      value={props.squares[i]}
+      onClick={() => props.onClick(i)}
     />;
   }
 
-  render() {
-
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 }
 
 class Game extends React.Component {
